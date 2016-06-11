94c48a6028de7e6ab62f986f4e42d98ac60decd7
shiba1014 <shiba.png@gmail.com>
2016-06-11 14:26:55 +0900

    Initial Commit

---
+++ SunsetAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunsetAlert
+//
+//  Created by Paul McCartney on 2016/06/11.
+//  Copyright © 2016年 shiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
